1c1a3cadda75d6ee3d2f4a349d589d81089e71a4
root <root@siva.(none)>
2013-12-05 09:42:48 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wall::Application.config.secret_token = 'c94a7e5ec5a5fc6520b94e97057e5fb98b58d13724ae665668b228505abd1b298721e900338d4c2b050db90ffe913872ccde1cf79d725dd0e201ffb9a6f8478d'
